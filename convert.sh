31732ab1d429155189f8c23bfc0b8b3386323a76
Brad Caron <bacaron@iu.edu>
2019-02-11 15:43:44 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -4,7 +4,7 @@ rawDT=`jq -r '.raw' config.json
 
 mkdir noddi;
 
-cp -v ./$rawDT/FIT_ICVF_NEW.nii.gz ./noddi/icvf.nii.gz;
-cp -v ./$rawDT/FIT_ISOVF_NEW.nii.gz ./noddi/isovf.nii.gz;
-cp -v ./$rawDT/FIT_OD_NEW.nii.gz ./noddi/od.nii.gz;
-cp -v ./$rawDT/FIT_dir.nii.gz ./noddi/dir.nii.gz;
+cp -v $rawDT/FIT_ICVF_NEW.nii.gz ./noddi/icvf.nii.gz;
+cp -v $rawDT/FIT_ISOVF_NEW.nii.gz ./noddi/isovf.nii.gz;
+cp -v $rawDT/FIT_OD_NEW.nii.gz ./noddi/od.nii.gz;
+cp -v $rawDT/FIT_dir.nii.gz ./noddi/dir.nii.gz;
